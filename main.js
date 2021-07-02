727aba8fb37f8de89bfb62063ac02cfde153ce5e
debosmita12 <76561588+debosmita12@users.noreply.github.com>
2021-07-02 15:32:09 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+menu_list_array = ["Veg Margherita","Peppy Paneer","Mexican Green Wave","Deluxe Veggie","Indi Tandoori Paneer","CHEESE N CORN","Veg Extravaganza"//add more items
+                    ];
+
+function getmenu(){
+var htmldata;
+htmldata="<ol class='menulist'>"
+menu_list_array.sort();
+for(var i=0;<i menu_list_array.length;i++){
+    htmldata=htmldata'<li>' + menu_list_array[i] + ;'<li>'//Complete the code
+}
+htmldata=htmldata+"<ol>"
+document.getElementById(display.menu).innerHTML=htmldata;
+function add_item(){
+var htmldata;
+var item=document.getElementById("add_item").value;
+menu_list_array.sort();
+htmldata="<section class='cards'>"
+for(var i=0;<i menu_list_array.length;i++){
+    htmldata=htmldata'<section class="cards">'
+    +<img src=image
+//Complete the code
+
+}
+
+function add_top(){
+//Complete the code
+}
